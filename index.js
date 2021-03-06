9b969a50ffabfc2f9568708cd7a33eb02dec1e81
Venika <venika.jarvis@gmail.com>
2021-03-05 21:24:13 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ document.querySelector('main').remove(); //1st test passed
 // can also be main.remove()
 
 
-// create a h1 element w/ the id name "victory"
+// create a h1 element
 const newHeader = document.createElement('h1'); //2nd test passed
+// append element variable to body to reflect in DOM
 document.body.appendChild(newHeader);
+//assign element variable's id name as "victory"
 newHeader.id = "victory";
-newHeader.innerHTML = "Nika is the champion"
+// add element to DOM using innerHTML w/ "Nika is the champion" inside of h1
+newHeader.innerHTML = "<h1>Nika is the champion</h1>"
 
 // const nodeName = document.createTextNode("");
 // newHeader.appendChild(nodeName);
