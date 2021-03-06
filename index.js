41852d8ecd71650ddf2ec27b6b998e405042defa
Venika <venika.jarvis@gmail.com>
2021-03-05 21:15:40 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+
+// remove <main> with id 'main'
+document.querySelector('main').remove(); //1st test passed
+// can also be main.remove()
+
+
+// create a h1 element w/ the id name "victory"
+const newHeader = document.createElement('h1'); //2nd test passed
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.innerHTML = "Nika is the champion"
+
+// const nodeName = document.createTextNode("");
+// newHeader.appendChild(nodeName);
+
